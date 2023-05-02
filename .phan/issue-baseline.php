961941e9efb4c5f994af55ef1ed64a5c82fc0e70
Máté Szabó <mszabo@fandom.com>
2023-05-02 14:28:51 +0200

    UGC-4131 | Unbreak extension
    In 50e64e5, the UsingData extension was refactored as part of upgrade
    work. This sadly broke the $sourcePage check in `getDataFrame`, causing
    it to render no data in the using tag and parser function. So, restore
    the original, working, condition, and add parser tests for the logic.
    
    Reproducing the issue in parser tests proved a bit nettlesome as the
    tests were falsely passing at first. This is because the UsingData
    parser hook handler instance uses shared state to store data frame
    instances, and this instance is kept alive and reused by all parser
    tests, causing data to be shared between tests that would not be shared
    between actual page parses. As such, introduce a ParserClearState
    handler to reset internal handler state between full parses to make the
    tests better reflect the behavior in real parsing. This in turn required
    removing the clearState() call and hooks manipulation in getDataFrame(),
    as it would also reset our clearState() handler. Given that direct
    manipulation of $wgHooks is deprecated as of MediaWiki 1.40 and clearing
    the set of handlers for a hook is only allowed in tests going forward,
    this is something we'd need to do sooner than later anyways.

---
+++ .phan/issue-baseline.php
@@ -0,0 +1,27 @@
+<?php
+/**
+ * This is an automatically generated baseline for Phan issues.
+ * When Phan is invoked with --load-baseline=path/to/baseline.php,
+ * The pre-existing issues listed in this file won't be emitted.
+ *
+ * This file can be updated by invoking Phan with --save-baseline=path/to/baseline.php
+ * (can be combined with --load-baseline)
+ */
+return [
+	// # Issue statistics:
+	// PhanUndeclaredProperty : 15+ occurrences
+	// PhanTypeMismatchReturn : 5 occurrences
+	// SecurityCheck-XSS : 3 occurrences
+	// PhanCoalescingNeverNull : 1 occurrence
+	// PhanTypeMismatchArgumentNullable : 1 occurrence
+	// PhanTypeMismatchReturnProbablyReal : 1 occurrence
+	// PhanUndeclaredMethod : 1 occurrence
+
+	// Currently, file_suppressions and directory_suppressions are the only supported suppressions
+	'file_suppressions' => [
+		'src/UsingDataHooks.php' => [ 'PhanCoalescingNeverNull', 'PhanTypeMismatchArgumentNullable', 'PhanUndeclaredProperty', 'SecurityCheck-XSS' ],
+		'src/UsingDataPPFrameDOM.php' => [ 'PhanTypeMismatchReturn', 'PhanTypeMismatchReturnProbablyReal', 'PhanUndeclaredMethod', 'PhanUndeclaredProperty' ],
+	],
+	// 'directory_suppressions' => ['src/directory_name' => ['PhanIssueName1', 'PhanIssueName2']] can be manually added if needed.
+	// (directory_suppressions will currently be ignored by subsequent calls to --save-baseline, but may be preserved in future Phan releases)
+];
