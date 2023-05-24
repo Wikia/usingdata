3d618bea29a873811f5d5d46b0eaedd1fbd2e254
Michał Chatłas <you@example.com>
2023-05-24 15:00:39 +0200

    Revert "Merge pull request #5 from Wikia/REL1_39"
    This reverts commit 74a04333c2b4e7c712b0a6af418fb13184514a61, reversing
    changes made to dedf2ac9bebed405134e74027460cc1d089865bb.

--- src/UsingDataPPFrameDOM.php
+++ src/UsingDataPPFrameDOM.php
@@ -1,11 +1,5 @@
 <?php
 
-namespace UsingData;
-
-use Parser;
-use PPFrame;
-use PPFrame_Hash;
-
 class UsingDataPPFrameDOM extends PPFrame_Hash {
 	public $parent; // Parent frame (either from #using or #data, providing a parser if needed), data source title
 	public $sourcePage;
@@ -71,9 +65,11 @@ class UsingDataPPFrameDOM extends PPFrame_Hash {
 			$this->expandedArgs = [];
 		}
 
-		$ret = is_string( $text ) && $useRTP ?
-			$this->expansionForParser->replaceVariables( $text, $this ) :
-			$this->expand( $text === null ? '' : $text );
+		if ( is_string( $text ) && $useRTP ) {
+			$ret = $this->expansionForParser->replaceVariables( $text, $this );
+		} else {
+			$ret = $this->expand( $text === null ? '' : $text );
+		}
 
 		$this->overrideArgs = $oldArgs;
 		$this->expansionFragment = $oldFragment;
@@ -110,7 +106,7 @@ class UsingDataPPFrameDOM extends PPFrame_Hash {
 					$text = $aar[1][$arg];
 					unset( $aar[1][$arg] );
 					$text = $aar[0]->expand( $text );
-					if ( str_contains( $text, Parser::MARKER_PREFIX ) ) {
+					if ( strpos( $text, Parser::MARKER_PREFIX ) !== false ) {
 						$text = $aar[0]->parser->serialiseHalfParsedText( ' ' . $text ); // MW bug 26731
 					}
 					$this->serializedArgs[$arg] = $text;
@@ -141,7 +137,7 @@ class UsingDataPPFrameDOM extends PPFrame_Hash {
 			case 'data-fragment':
 				return $this->expansionFragment;
 			case 'data-source-fragment':
-				return $this->sourcePage . ( empty( $this->expansionFragment ) ? '' : "#$this->expansionFragment" );
+				return $this->sourcePage . ( empty( $this->expansionFragment ) ? '' : ( '#' . $this->expansionFragment ) );
 			default:
 				if ( is_array( $this->overrideArgs ) && isset( $this->overrideArgs[$index] ) ) {
 					if ( is_object( $this->overrideArgs[$index] ) ) {
@@ -149,7 +145,7 @@ class UsingDataPPFrameDOM extends PPFrame_Hash {
 					}
 					return $this->overrideArgs[$index];
 				}
-				$p = $this->expansionForParser ?? $this->parent->parser;
+				$p = $this->expansionForParser === null ? $this->parent->parser : $this->expansionForParser;
 				return $this->getArgumentForParser( $p, $this->expansionFragmentN, $index, false );
 		}
 	}
